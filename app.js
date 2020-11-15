57fae7de9c6f4795616589ecb56061115547861f
Dan Sehl <dan.sehl@gmail.com>
2020-11-15 09:02:20 -0700

    Update app.js with error handling

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -20,8 +21,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use((req, res, next) => {
-  res.status(404).render('404');
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
 // error handler
