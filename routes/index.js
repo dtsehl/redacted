6783fe43acb02ac393c305b1e037dc50dff442a0
Dan Sehl <dan.sehl@gmail.com>
2020-11-16 16:01:50 -0700

    Remove unnecessary routes from index.js

--- routes/index.js
+++ routes/index.js
@@ -6,27 +6,11 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false })
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   res.render('index', {
-    title: 'Redacted',
+    title: 'Redacted'
   });
 });
 
-router.post('/', urlencodedParser, (req, res) => {
-  replacer(req);
-});
-
-function replacer(input) {
-  let output = input.body.name
-  let splitInput = input.body.words.match(/\w+|"[^"]+"/g);
-  splitInput.forEach((x, y) => {
-    splitInput[y] = x.split('"').join('')
-  })
-  console.log(splitInput)
-  splitInput.forEach(x => {
-    output = output.split(x).join('XXXX')
-  })
-  console.log(output)
-}
-
 module.exports = router;
+
